c3e4f5f787326d755e3c396d28366f23a38c4cd5
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-06-08 00:18:32 +1000

    added task 3

---
+++ homework/maksim_ivanov/Homework_03/average.py
@@ -0,0 +1,14 @@
+x = 26
+y = 150
+
+
+def average(a, b):
+    return (a + b)/2
+
+
+def geometric_mean(a, b):
+    return (a*b)**(1/2)
+
+
+print(f'Среднее арифметическое {x} и {y} = {average(x, y)}\n'
+      f'Среднее геометрическое {x} и {y} = {geometric_mean(x, y)}')
