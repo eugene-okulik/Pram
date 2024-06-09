49fe35e89b324b0c96673ce4ab9da2fa133b6c5d
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-06-09 13:06:14 +1000

    added my_dict and actions

---
+++ homework/maksim_ivanov/Homework_04/dict.py
@@ -0,0 +1,28 @@
+my_dict = {
+    'tuple': (1, 3, 6, 8, 4, 7, 2),
+    'list': [1, 2, 4, 5, 8, 9],
+    'dict': {
+        '1': (2, 'A'),
+        '3': 3,
+        4: [4, 7, 8],
+        'xy': 'x1y1',
+        5: 5,
+        7: 111
+        },
+    'set': {1, 6, 5, 'A', 'Qwerty', 8}
+    }
+
+for key, value in my_dict.items():
+    if key == 'tuple':
+        print('Последний элемент tuple: ' + str(value[-1]))
+    elif key == 'list':
+        value.append(22)
+        value.pop(1)
+    elif key == 'dict':
+        value['i am a tuple'] = 'tuple'
+        value.pop('3')
+    elif key == 'set':
+        value.add(56)
+        value.remove(5)
+
+print(my_dict)
