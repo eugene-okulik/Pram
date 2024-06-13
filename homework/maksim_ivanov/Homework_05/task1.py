91636e4785bf48f2b685eb80896da36050f8f9a3
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-06-13 15:23:35 +1000

    added task 1

---
+++ homework/maksim_ivanov/Homework_05/task1.py
@@ -0,0 +1,10 @@
+person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
+name, last_name, city, phone, country = person
+
+print(person)
+print(f'{name=}\n'
+      f'{last_name=}\n'
+      f'{city=}\n'
+      f'{phone=}\n'
+      f'{country=}'
+      )
