67d201913f4fdf93b851a2533ccd99f822162aff
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-10-01 14:07:16 +1000

    added annotations

--- homework/maksim_ivanov/Homework_13/task1.py
+++ homework/maksim_ivanov/Homework_13/task1.py
@@ -6,10 +6,8 @@ base_path = os.path.dirname(__file__)
 homework_path = os.path.dirname(os.path.dirname(base_path))
 data_file_path = os.path.join(homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
 
-days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-
-def read_file(file_path: str) -> str:
+def read_file(file_path: str):
     with open(file_path, 'r', encoding='utf-8') as file:
         for line in file.readlines():
             yield line
@@ -27,7 +25,7 @@ for line in read_file(data_file_path):
     if 'распечатать эту дату, но на неделю позже.' in operation:
         result = date + datetime.timedelta(weeks=1)
     elif 'распечатать какой это будет день недели' in operation:
-        result = days[date.weekday()]
+        result = date.strftime("%A")
     elif 'распечатать сколько дней назад была эта дата' in operation:
         result = (datetime.date.today() - date.date()).days
     print(result)
