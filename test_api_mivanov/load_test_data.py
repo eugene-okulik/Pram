c4d4231024e728cfe8d1ff1a897d51d63eb773b3
Maksim Ivanov <maksimivanov.06@yandex.ru>
2024-11-15 19:03:30 +1000

    edited loading test data

---
+++ test_api_mivanov/load_test_data.py
@@ -0,0 +1,7 @@
+from helpers import load_data
+
+test_data_create = load_data("create_object_positive_test_data.json")
+test_data_create_negative = load_data("create_object_negative_test_data.json")
+test_data_one_object = load_data("create_one_object_test_data.json")
+test_data_put = load_data("put_object_positive_test_data.json")
+test_data_patch = load_data("patch_object_positive_test_data.json")
--- test_api_mivanov/tests/tests.py
+++ test_api_mivanov/tests/tests.py
@@ -1,8 +1,7 @@
 import pytest
 
-from helpers import load_data
-
-test_data_create = load_data("create_object_positive_test_data.json")
+from load_test_data import (test_data_create, test_data_create_negative, test_data_one_object, test_data_put,
+                            test_data_patch)
 
 
 @pytest.mark.parametrize("test_data_create", test_data_create)
@@ -12,17 +11,10 @@ def test_create_object(create_object, test_data_create):
     create_object.check_output_data(payload=test_data_create)
 
 
-test_data_create_negative = load_data("create_object_negative_test_data.json")
-
-
 @pytest.mark.parametrize("test_data_create_negative", test_data_create_negative)
 def test_create_object_with_negative_data(create_object, test_data_create_negative):
     create_object.create_new_object(payload=test_data_create_negative)
     create_object.check_that_status_is_400()
-
-
-test_data_one_object = load_data("create_one_object_test_data.json")
-test_data_put = load_data("put_object_positive_test_data.json")
 
 
 @pytest.mark.parametrize("test_data_one_object", test_data_one_object)
@@ -32,9 +24,6 @@ def test_put_object(create_object, put_object, test_data_one_object, test_data_p
     put_object.put_update_object(payload=test_data_put, obj_id=create_object.obj_id)
     put_object.check_that_status_is_200()
     put_object.check_output_data(payload=test_data_put)
-
-
-test_data_patch = load_data("patch_object_positive_test_data.json")
 
 
 @pytest.mark.parametrize("test_data_one_object", test_data_one_object)
